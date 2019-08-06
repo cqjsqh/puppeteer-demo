af25fee9e72aef92effc5cbcb285559bd9a2bcea
qijie chen <qijie.chen@yintech.cn>
2019-08-06 16:32:50 +0800

    pdf

--- pdf.js
+++ pdf.js
@@ -7,6 +7,7 @@ const puppeteer = require('puppeteer');
   //await page.setViewport({width:1920, height:1080});
   await page.goto("https://www.baidu.com");
   
+  await page.waitForSelector('body');
   await page.pdf({
     path: 'dist/baidu.pdf',
     format: 'A4',
